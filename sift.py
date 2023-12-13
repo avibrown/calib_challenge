f865095073c76d9da171f4c214d2339d00cd5b53
avibrown <abrown.inbox@gmail.com>
2023-12-13 11:52:39 +0200

    flux

--- sift.py
+++ sift.py
@@ -1,24 +1,63 @@
 import cv2
 import numpy as np
+from sklearn.cluster import DBSCAN
 
 vid = "./labeled/1.hevc"
 cap = cv2.VideoCapture(vid)
 
+feature_params = dict(maxCorners=1000, qualityLevel=0.00001, minDistance=10, blockSize=9)
+lk_params = dict(winSize=(15, 15), maxLevel=10, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+ret, old_frame = cap.read()
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+
+scaling_factor = 30
+percent = 50  # Focus on the fastest 5%
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-    dst = cv2.dilate(dst, None)
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    frame[dst > 0.001*dst.max()] = [0, 0, 255]
+    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    good_new = p1[st == 1]
+    good_old = p0[st == 1]
+
+    # Calculate velocities
+    velocities = np.sqrt((good_new[:, 0] - good_old[:, 0])**2 + (good_new[:, 1] - good_old[:, 1])**2)
+    velocity_threshold = np.percentile(velocities, percent)
+
+    fast_corners = []
+
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        a, b, c, d = int(a), int(b), int(c), int(d)
+        if velocities[i] >= velocity_threshold:
+            fast_corners.append((a, b))
+
+    if fast_corners:
+        # Use DBSCAN clustering to find the densest area of fast-moving corners
+        clustering = DBSCAN(eps=50, min_samples=10).fit(fast_corners)
+        labels = clustering.labels_
+
+        # Find the largest cluster
+        largest_cluster_index = max(set(labels), key=list(labels).count)
+        largest_cluster = np.array(fast_corners)[labels == largest_cluster_index]
+
+        if len(largest_cluster) > 0:
+            (x, y), radius = cv2.minEnclosingCircle(largest_cluster)
+            frame = cv2.circle(frame, (int(x), int(y)), int(radius), (0, 0, 255), 2)  # Draw enclosing circle
+            frame = cv2.circle(frame, (int(x), int(y)), 10, (0, 0, 255), -1)  # Draw center dot
 
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
+
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
 
 cap.release()
 cv2.destroyAllWindows()
