e5e39de647f19f67e4cffe8da51b4a41f2e08ea4
avibrown <abrown.inbox@gmail.com>
2023-12-13 08:34:31 +0200

    test

--- sift.py
+++ sift.py
@@ -1,8 +1,24 @@
 import cv2
+import numpy as np
 
-vid = "./labeled/0.hevc"
+vid = "./labeled/1.hevc"
 cap = cv2.VideoCapture(vid)
 
 
 while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv2.dilate(dst, None)
+
+    frame[dst > 0.001*dst.max()] = [0, 0, 255]
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
